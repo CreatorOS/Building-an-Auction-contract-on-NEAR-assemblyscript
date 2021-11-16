d59dc6939ffbac2c846198bbeb0b1b364a0b5e80
hadi <hadi.mohammad.saleh1@gmail.com>
2021-11-16 18:59:25 +0300

    getting use-contract to work

--- learn_src/scripts/2.use-contract.sh
+++ learn_src/scripts/2.use-contract.sh
@@ -8,6 +8,7 @@ set -e
 #near view $CONTRACT.hdsaleh.testnet getBids
 #near view $CONTRACT getBids
 #near call $CONTRACT.hdsaleh.testnet createAuction '{"biddingTime": "1000"}' --account_id $CONTRACT.hdsaleh.testnet
-#near call $CONTRACT createAuction '{"biddingTime" : "1000"}' --accountId $CONTRACT
+near call $CONTRACT.hdsaleh.testnet createAuction '{"biddingTime" : 1000}' --accountId $CONTRACT.hdsaleh.testnet
+
 near view $CONTRACT.hdsaleh.testnet getBids
 exit 0
